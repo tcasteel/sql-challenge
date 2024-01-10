d1dbb17346b55e41e576bf42c435617a577d56ab
tcasteel <144496761+tcasteel@users.noreply.github.com>
2024-01-10 09:51:13 -0600

    updating SQL Challenge code

--- SQL_Challenge.sql
+++ SQL_Challenge.sql
@@ -1,3 +1,53 @@
+CREATE TABLE departments (
+	pk PRIMARY KEY,
+    dept_no VARCHAR(10),
+    dept_name VARCHAR(100),
+);
+
+CREATE TABLE dept_emp (
+	pk PRIMARY KEY,
+    emp_no INT,
+    dept_no VARCHAR(10),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+);
+
+CREATE TABLE dept_manager (
+	pk PRIMARY KEY,
+    dept_no VARCHAR(10),
+    emp_no INT,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+);
+
+CREATE TABLE employees (
+	pk PRIMARY KEY,
+    emp_no VARCHAR(50),
+	emp_title_id VARCHAR(50),
+	birth_date DATE,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    sex CHAR(1),
+    hire_date DATE,
+);
+
+CREATE TABLE salaries (
+	pk PRIMARY KEY,
+    emp_no VARCHAR(50),
+    salary INT,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+);
+
+CREATE TABLE titles (
+	PK PK INT,
+	title_id VARCHAR(50),
+	title VARCHAR(50)
+);
+	
+	
+	
+	
+	
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 JOIN salaries s ON e.emp_no = s.emp_no;
