9f395676b0e34c1f356843c36ffa095bda28a4ba
tcasteel <144496761+tcasteel@users.noreply.github.com>
2024-02-15 19:44:15 -0600

    Add files via upload

--- SQL_Challenge.sql
+++ SQL_Challenge.sql
@@ -1,89 +1,41 @@
 CREATE TABLE departments (
-	pk PRIMARY KEY,
-    dept_no VARCHAR(10),
-    dept_name VARCHAR(100),
+    dept_no VARCHAR(10) PRIMARY KEY,
+    dept_name VARCHAR(100)
 );
 
 CREATE TABLE dept_emp (
-	pk PRIMARY KEY,
     emp_no INT,
     dept_no VARCHAR(10),
+    PRIMARY KEY (emp_no, dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager (
-	pk PRIMARY KEY,
     dept_no VARCHAR(10),
     emp_no INT,
+    PRIMARY KEY (dept_no, emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE employees (
-	pk PRIMARY KEY,
-    emp_no VARCHAR(50),
-	emp_title_id VARCHAR(50),
-	birth_date DATE,
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR(50),
+    birth_date DATE,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     sex CHAR(1),
-    hire_date DATE,
+    hire_date DATE
 );
 
 CREATE TABLE salaries (
-	pk PRIMARY KEY,
-    emp_no VARCHAR(50),
+    emp_no INT PRIMARY KEY,
     salary INT,
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE titles (
-	PK PK INT,
-	title_id VARCHAR(50),
-	title VARCHAR(50)
+    title_id VARCHAR(50) PRIMARY KEY,
+    title VARCHAR(50)
 );
-	
-	
-	
-	
-	
-SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
-FROM employees e
-JOIN salaries s ON e.emp_no = s.emp_no;
-
-SELECT first_name, last_name, hire_date
-FROM employees
-WHERE EXTRACT(YEAR FROM hire_date) = 1986;
-
-SELECT dm.dept_no, d.dept_name, dm.emp_no AS manager_emp_no, e.last_name AS manager_last_name, e.first_name AS manager_first_name
-FROM dept_manager dm
-JOIN employees e ON dm.emp_no = e.emp_no
-JOIN departments d ON dm.dept_no = d.dept_no;
-
-SELECT e.emp_no, e.last_name, e.first_name, de.dept_no, d.dept_name
-FROM employees e
-JOIN dept_emp de ON e.emp_no = de.emp_no
-JOIN departments d ON de.dept_no = d.dept_no;
-
-SELECT first_name, last_name, sex
-FROM employees
-WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
-
-SELECT e.emp_no, e.last_name, e.first_name
-FROM employees e
-JOIN dept_emp de ON e.emp_no = de.emp_no
-JOIN departments d ON de.dept_no = d.dept_no
-WHERE d.dept_name = 'Sales';
-
-SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
-FROM employees e
-JOIN dept_emp de ON e.emp_no = de.emp_no
-JOIN departments d ON de.dept_no = d.dept_no
-WHERE d.dept_name IN ('Sales', 'Development');
-
-SELECT last_name, COUNT(*) AS frequency_count
-FROM employees
-GROUP BY last_name
-ORDER BY frequency_count DESC;
-
---
+++ SQL_Challengept2.sql
@@ -0,0 +1,38 @@
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no;
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+
+SELECT dm.dept_no, d.dept_name, dm.emp_no AS manager_emp_no, e.last_name AS manager_last_name, e.first_name AS manager_first_name
+FROM dept_manager dm
+JOIN employees e ON dm.emp_no = e.emp_no
+JOIN departments d ON dm.dept_no = d.dept_no;
+
+SELECT e.emp_no, e.last_name, e.first_name, de.dept_no, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no;
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
+SELECT last_name, COUNT(*) AS frequency_count
+FROM employees
+GROUP BY last_name
+ORDER BY frequency_count DESC;
